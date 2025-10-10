424d0bbb3d2cd7ed58e254957033dd7d5c79c1e6
Ratnakar <ratnakarkolhatkar@gmail.com>
2025-10-10 22:34:06 +0530

    Maven Commands to build and execute the test suite

--- src/bdd/java/com/ratnakar/framework/StandAloneEcommerceWebTest.java
+++ src/bdd/java/com/ratnakar/framework/StandAloneEcommerceWebTest.java
@@ -2,19 +2,21 @@ package com.ratnakar.framework;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import net.thucydides.core.annotations.findby.By;
-import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 import java.time.Duration;
 import java.util.List;
 
 public class StandAloneEcommerceWebTest {
-    public static void main(String[] args) throws InterruptedException {
+    @Test
+    public void EcommerceWebAppTest() throws InterruptedException {
         // Setup ChromeDriver automatically using WebDriverManager.
         // It downloads the right version if not available and configures system property internally.
         WebDriverManager.chromedriver().setup();
