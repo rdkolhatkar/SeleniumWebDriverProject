2dfe19c6580d58bef147486803ef9f8d557eb035
Ratnakar <ratnakarkolhatkar@gmail.com>
2026-01-24 20:55:39 +0530

    Excel Data Driven Test

--- src/test/java/com/selenium/test/DataProviderTest.java
+++ src/test/java/com/selenium/test/DataProviderTest.java
@@ -1,5 +1,7 @@
 package com.selenium.test;
 
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -9,6 +11,7 @@ import org.testng.annotations.Test;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class DataProviderTest {
     @DataProvider(name = "driveTestDataWithExcel")
@@ -28,10 +31,27 @@ public class DataProviderTest {
         // Here we are mentioning the [rowCount -1] & [columnCount -1] because we have to skip first row as it contains the headers not the actual values
         Object[][] data = new Object[rowCount -1][columnCount -1];
         // Now we have to write the two for loops first is iteration over the rows and inner for loop will iterate over the columns for each row
+        for(int i = 0; i<rowCount-1; i++){
+            System.out.println("Outer loop started to get the rows by their row index values");
+            row = sheet.getRow(i+1);// i+1 is written because we need to skip the first row as it contains the column titles and headers
+            for(int j = 0; j<columnCount-1; j++){
+                System.out.println("Inner loop started to get the Cells by their column index values");
+                // But in an excel sheet there can be different data values like Integer value, String Value etc.
+                // We have to fetch each cell value and convert it into a String before inserting it into multidimensional data[][] array
+                // Here we will use DataFormatter class for converting data into String
+                DataFormatter dataFormatter = new DataFormatter();
+                XSSFCell cellValue = row.getCell(j);// Fetching each cell value from 'i' th index row ans Storing it into variable
+                System.out.println(cellValue);
+                data[i][j] = dataFormatter.formatCellValue(cellValue); // Storing the extracted data into an multidimensional array
+                System.out.println("Printing the Multidimensional array");
+                System.out.println(Arrays.deepToString(data));
+            }
+        }
         return data;
     }
-    @Test
-    public void testCaseDataProvider(){
-
+    @Test(dataProvider = "driveTestDataWithExcel")
+    public void testCaseDataProvider() throws IOException {
+        DataProviderTest dataProviderTest = new DataProviderTest();
+        dataProviderTest.getData();
     }
 }
